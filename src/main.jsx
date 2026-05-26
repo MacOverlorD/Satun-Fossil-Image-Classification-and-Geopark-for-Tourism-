c41ac2a9900e9f492a68f0b3aac5bd680113a507
MacOverlorD <lappawatlaohasoot@gmail.com>
2026-05-26 19:45:52 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FossilDex from "./FossilDex.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<FossilDex />);
